0926e0673f700121c7c04d9d49f0138330ab5902
juanpedropuig <67132645+juanpedropuig@users.noreply.github.com>
2020-11-14 13:46:52 -0300

    Change name from main to script

---
+++ script.js
@@ -0,0 +1,12 @@
+// Fortune teller 
+/* Output example: 
+You may meet your love of your life this week
+It's gonna be in your next visit to the doctor
+Look out for a fatty with glasses */
+
+// Horoscope generator
+/* Output example:
+Your sign is the sun.
+You're having good luck.
+You should: 'trust no one'. */
+
