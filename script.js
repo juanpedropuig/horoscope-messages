a2a975d26808dff13a18525f3b8778266050a786
juanpedropuig <67132645+juanpedropuig@users.noreply.github.com>
2020-11-14 14:40:05 -0300

    Add message components

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-// Fortune teller 
-/* Output example: 
-You may meet your love of your life this week
-It's gonna be in your next visit to the doctor
-Look out for a fatty with glasses */
-
 // Horoscope generator
 /* Output example:
 Your sign is the sun.
 You're having good luck.
 You should: 'trust no one'. */
+
+// Message components.
+const solarSystemStars = ['the sun', 'mercury', 'venus', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune'];
+const fate = [
+  `You're having good luck`,
+  `Someone near you will betray you`,
+  `You're going to have a fight with a close friend`,
+  `You're not having good luck at all.`,
+  `You have lots of questions without answering.`,
+  `A great love of your life will leave you.`,
+  `You are about to find the love of your life.`
+  ];
+const youShould = [
+  'trust no one.',
+  'be careful with what you say this week.',
+  'be more trustworthy.',
+  'check your inner circle for jealousy.',
+  'question your life decision.',
+  'question who you trust.',
+  `be careful who's always hearing.`,
+  'spend more money on your love ones.',
+  'not worry about horoscopes.',
+  'believe more in yourself.'
+];
+
 
