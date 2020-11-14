4f36854a729180fc53e76bf25f4c6aa232620805
juanpedropuig <67132645+juanpedropuig@users.noreply.github.com>
2020-11-14 15:27:22 -0300

    Add functionality with the read method

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 // Horoscope generator
-/* Output example:
-Your sign is the sun.
-You're having good luck.
-You should: 'trust no one'. */
-
-// Message components.
 const Horoscope = {
-  solarSystemStars: ['the sun', 'mercury', 'venus', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune'],
-  fate: [
+  signs: ['the sun', 'mercury', 'venus', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'kwik-e-mart', 'krusty clown'],
+  fates: [
   `You're having good luck`,
   `Someone near you will betray you`,
   `You're going to have a fight with a close friend`,
   `You're not having good luck at all.`,
   `You have lots of questions without answering.`,
   `A great love of your life will leave you.`,
-  `You are about to find the love of your life.`
+  `You are about to find the love of your life.`,
+  `You're going to let a great opportunity slip out of your hands.`,
+  `You're going to discover a great power inside you.`
   ],
-  youShould: [
+  actions: [
   'trust no one.',
   'be careful with what you say this week.',
   'be more trustworthy.',
@@ -26,7 +22,26 @@ const Horoscope = {
   `be careful who's always hearing.`,
   'spend more money on your love ones.',
   'not worry about horoscopes.',
-  'believe more in yourself.'
-  ]
+  'believe more in yourself.',
+  'not believe everything you hear.'
+  ],
+
+  // Reads the horoscope and outputs the results.
+  read() {
+    // Randomly selects a number within the range of each array.
+    const signIndex = Math.floor(Math.random() * this.signs.length);
+    const fateIndex = Math.floor(Math.random() * this.fates.length);
+    const actionIndex = Math.floor(Math.random() * this.actions.length);
+  
+    // Finds each value in its corresponding array.
+    const sign = this.signs[signIndex];
+    const fate = this.fates[fateIndex];
+    const action = this.actions[actionIndex];
+
+    console.log(`Your sign is ${sign}.`);
+    console.log(`${fate}`);
+    console.log(`You should ${action}`);
+  }
 }
 
+Horoscope.read();
